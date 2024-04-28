63b86e083de68e84d545fcda6a74895c63375723
akande56 <abdulsalamabubakar52@gmail.com>
2024-04-28 08:48:31 +0100

    import APIVIEW

--- taxapp2/users/views.py
+++ taxapp2/users/views.py
@@ -4,6 +4,7 @@ from rest_framework.request import Request
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from django.core.mail import send_mail
 from .models import User
 from .permissions import IsUserOrReadOnly
